48d4c6665d25ea4d6e1bb2e8e47ba42b74524455
thiagovsorio <thiagovsorio@gmail.com>
2023-07-17 22:45:02 -0300

    refact(directories): "archiving" older versions

--- boot.asm
+++ boot.asm
@@ -1,13 +1,69 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
+_start:
+    jmp short start
+    nop
+
+times 33 db 0 ; filling the future bios parameter block with zeros
+
 start:
+    jmp 0x7c0:step2 ; skiping bios parameter block
+    ; updates the code segment to 0x7c0, where our step2 starts
+
+handle_zero:
     mov ah, 0eh
     mov al, 'A'
-    mov bx, 0
+    mov bx, 0x00
     int 0x10
+    iret
 
+handle_one:
+    mov ah, 0eh
+    mov al, 'V'
+    mov bx, 0x00
+    int 0x10
+    iret
+
+step2:
+    cli ; Clear Interrupts
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ; Enables Interrupts
+
+    mov word[ss:0x00], handle_zero
+    mov word[ss:0x02], 0x7c0
+
+    mov word[ss:0x04], handle_one
+    mov word[ss:0x06], 0x7c0
+
+    int 1
+
+    mov si, message ; message is a label
+    call print ; and when you reference a label you get the address
     jmp $
 
+print:
+    mov bx, 0
+.loop:
+    lodsb ; loads the info on address of si to al and increment si
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret ; ret returns to subroutines and call is a subroutine
+
+print_char:
+    mov ah, 0eh
+    int 0x10
+    ret
+
+message: db 13,10,13,10,"Booting on my friend's computer, so hopefully it didn't catch fire",13,10, 0
 times 510-($ - $$) db 0
 dw 0xAA55
